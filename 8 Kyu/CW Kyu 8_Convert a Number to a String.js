a7f13f70a1b0a840317d5e704000eb24ddd7cff9
marwankhalife <maljeburi@gmail.com>
2022-06-10 17:29:17 +0200

    adding new CW challanges

---
+++ 8 Kyu/CW Kyu 8_Convert a Number to a String.js
@@ -0,0 +1,32 @@
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+
+// 123 --> "123"
+// 999 --> "999"
+
+// SOLUTIONS ======================
+
+function numberToString(num) {
+    return num.toString();
+  }
+
+// OR
+
+function numberToString(num) {
+    return String(num);
+  }
+
+// OR
+
+function numberToString(num) {
+    return ''+num;
+  }
+
+// OR
+
+function numberToString(num) {
+    return ''+num;
+  }
---
+++ 8 Kyu/CW Kyu 8_Convert a String to a Number.js
@@ -0,0 +1,39 @@
+// Description
+
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+// Examples
+
+// "1234" --> 1234
+// "605"  --> 605
+// "1405" --> 1405
+// "-7" --> -7
+
+//SOLUTION S ===================
+
+const stringToNumber = function(str){
+    return Number(str)
+  };
+
+  // OR
+
+  var stringToNumber = function(str){
+    return parseInt(str);
+  };
+
+  // OR
+
+  var stringToNumber = function(str){
+    return +str;
+  };
+
+
+  // OR
+
+  const stringToNumber = str => Number(str);
+
+  // OR
+
+  var stringToNumber = Number;
