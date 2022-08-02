fa5d5482edfd54cd59346a17c1932ee887b392c3
marwankhalife <maljeburi@gmail.com>
2022-08-02 22:00:37 +0200

    new CW chal

---
+++ 8 Kyu/CW Kyu 8_Convert a String to an Array.js
@@ -0,0 +1,41 @@
+/* Write a function to split a string and convert it into an array of words.
+
+Examples (Input ==> Output):
+
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"] */
+
+// SOLUTIONS ============================================
+
+function stringToArray(string){
+    return string.split(' ');
+}
+
+// .split returns an array of items depending on the split sparator " " space in this example, if the split is not defined '', then the entire string will be terurned.
+
+// OR 
+
+const stringToArray = string => string.split(' ');
+
+// OR
+
+function stringToArray(string){
+    var result = [];
+    var traverse = function foo(string){
+        for(var i=0;i<string.length;i++){
+            var chr = string.charAt(i);
+            if(chr === " "){
+                result.push(string.slice(0,i));
+                foo(string.slice(i+1));
+                return;
+            }
+        }
+        result.push(string);
+    };
+    traverse(string);
+    return result;
+}
+
+// OR
+
